fda87cb0088f6368db80ee7b66162886412f388b
Adhithi Maran <adhithimaran@gmail.com>
2026-03-07 17:53:03 -0600

    current

--- app/src/main/java/com/example/cribswap/MainActivity.kt
+++ app/src/main/java/com/example/cribswap/MainActivity.kt
@@ -4,19 +4,16 @@ import com.example.cribswap.ui.theme.Filter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cribswap.ui.theme.CribSwapTheme
+
+// State Management Imports
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
@@ -30,7 +27,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CribSwapTheme {
-                val filters = listOf("Distance", "Price", "Lease Term", "# of Beds", "# of Baths", "Amenities Included", "In-Unit Laundry")
+                val filters = listOf(
+                    "Distance",
+                    "Price",
+                    "Lease Term (Start Month - End Month)",
+                    "Bedrooms",
+                    "Bathrooms",
+                    "Amenities Included",
+                    "In-Unit Laundry"
+                )
                 var selectedFilters by remember { mutableStateOf(setOf("All")) }
                 var isFilterVisible by remember { mutableStateOf(false) }
                 Column {

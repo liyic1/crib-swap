a1d85c8ade76e16c969d5d944e8bf5a3fcfccda4
Adhithi Maran <adhithimaran@gmail.com>
2026-03-09 02:05:58 -0500

    Add: navItem data class + Navigation bar

--- app/src/main/java/com/example/cribswap/Explore.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cribswap
-
-class Explore {
-}
--- app/src/main/java/com/example/cribswap/Home.kt
+++ app/src/main/java/com/example/cribswap/Home.kt
@@ -10,19 +10,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.mutableIntSetOf
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Home(modifier: Modifier = Modifier) {
+    val NavItemList = listOf(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Saved", Icons.Default.Favorite),
+        NavItem("Chat", Icons.Default.Create),
+        NavItem("Profile", Icons.Default.Face)
+    )
+    var selectedIndex by remember {
+        mutableIntStateOf(0)
+    }
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
-             Text(text= "bottom")
+            NavigationBar {
+                NavItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, contentDescription = "Icon")
+                        },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+                }
+            }
         }
     ) { innerPadding ->
         ContentScreen(modifier = Modifier.padding(innerPadding))

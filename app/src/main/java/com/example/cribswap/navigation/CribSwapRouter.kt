ab864f78c42d2dcf5805e4390cf668c41f67e814
FarhiyaKhalif <farhiyakhalif843@gmail.com>
2026-04-26 12:54:03 -0500

    included messaging screen object

--- app/src/main/java/com/example/cribswap/navigation/CribSwapRouter.kt
+++ app/src/main/java/com/example/cribswap/navigation/CribSwapRouter.kt
@@ -19,6 +19,7 @@ sealed class Screen {
     object PrivacyPolicyScreen : Screen()
     object TermsScreen : Screen()
     object MainScreen : Screen() // post-login: nav bar and filter
+    object MessagesScreen : Screen()
 }
 
 object CribSwapAppRouter {
